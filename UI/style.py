7c46a6b74e8aa42cb6bcbe3de2f71bbe3b35cc88
Ihogan-tech <ian@ianhogan.com>
2025-05-09 13:48:11 -0500

    Add files via upload

---
+++ UI/style.py
@@ -0,0 +1,44 @@
+# Define colors, fonts, and other style-related constants here.
+
+# Colors
+BACKGROUND_COLOR = '#202A44'
+SIDE_FRAME_COLOR = '#293B5A'
+BUTTON_BG_COLOR = 'light blue'
+TEXT_COLOR = 'white'
+BUTTON_HIGHLIGHT_COLOR = 'light blue'
+
+# Fonts
+FONT_LARGE = 'Helvetica 16 bold'
+FONT_MEDIUM = 'Helvetica 12 bold'
+
+# Entry styles
+ENTRY_STYLES = {
+    'bg': 'white',  # Sets the entry background color
+    'fg': 'black',  # Sets the entry text color
+    'insertbackground': 'black',  # Sets the color of the insertion cursor
+    'font': 'Helvetica 12'  # Sets the font and size of the text
+}
+
+
+# Button styles
+BUTTON_STYLES = {
+    'bg': 'light blue',
+    'fg': 'black',  # Sets the button text color
+    'width': 15,
+    'relief': 'solid',
+    'bd': 1,
+    'highlightbackground': 'light blue',
+    'highlightthickness': 10,
+    'font': 'Helvetica 12 bold'
+}
+
+# Label styles
+LABEL_STYLES = {
+    'bg': BACKGROUND_COLOR,
+    'fg': TEXT_COLOR,
+    'font': FONT_LARGE
+}
+
+# Padding styles
+PAD_Y = (10, 10)
+PAD_X = (10, 10)
